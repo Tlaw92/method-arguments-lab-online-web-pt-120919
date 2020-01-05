7690a6fd4173a0a0bd22af7446ef8377f2ddb999
travis Lawson <travislawclimb@gmail.com>
2020-01-05 20:02:38 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,19 @@
-def introduction(name)
-  puts "Hi, my name is #{name}."
+
+
+def introduction_with_language("name","language")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
+
+introduction_with_language("Ben", "ruby")
+
+#def introduction_with_language(name,language)
+  #puts "Hello #{name}, we heard you are an amazing #{language} programmer!"
+#end
+
+#introduction_with_language("Sophie","ruby")
+
+
+#def introduction("Sophie")
+# puts "Hi, my name is #{name}."
+#end
+
